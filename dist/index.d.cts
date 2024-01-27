05501fbb117b25f4e35c9a118ea0447fbb36fefd
panxiaoliang <panxiaoliang>
2024-01-27 15:11:59 +0800

    fix: 删除 dist

--- dist/index.d.cts
+++
@@ -1,5 +0,0 @@
-declare const add: (a: number, b: number) => number;
-
-declare const subtract: (a: number, b: number) => number;
-
-export { add, subtract };
--- dist/index.mjs
+++
@@ -1,9 +0,0 @@
-const subtract = (a, b) => a - b;
-
-const add = (a, b) => {
-  return a + b;
-};
-
-console.log(add(1, 3), subtract(1, 6));
-
-export { add, subtract };
