c8c8fb3a308a02128746ec7db47c9c6ecdb8bf5c
panxiaoliang <panxiaoliang>
2024-01-27 15:10:23 +0800

    fix: init

--- mod.ts
+++
@@ -1,6 +0,0 @@
-import { subtract } from "./deps/subtract.ts";
-import { add } from "./deps/add.ts";
-export { add } from "./deps/add.ts";
-export { subtract } from "./deps/subtract.ts";
-
-console.log(add(1, 3), subtract(1, 3));
